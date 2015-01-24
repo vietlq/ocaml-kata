b47bf279b745feb2e12a3e9029b00c995f2d5a73
Le Quoc Viet <vietlq85@gmail.com>
2015-01-24 23:19:33 +0000

    Added the comments

--- prob011_modified_run_length_encode.ml
+++ prob011_modified_run_length_encode.ml
@@ -11,14 +11,25 @@ type 'a rle =
     | Many of int * 'a;;
 
 let encode l =
+    (* Utility function to merge the accumulator
+        when the count of a value is known *)
     let merge count x acc = if count > 1
         then Many (count, x)::acc
         else One x::acc
+    (* The actual recursive function *)
     in let rec aux count acc = function
+        (* When the list is empty, return the accumulator *)
         | [] -> acc
+        (* When processing the last element of the list, merge *)
         | [x] -> merge (count+1) x acc
+        (* When the list has at least 2 elements,
+            compare the head and the next *)
         | h::(x::_ as t) -> if h = x
+            (* If forward-looking is positive,
+                keep going, don't merge yet *)
             then aux (count+1) acc t
+            (* If forward-looking is negative,
+                do the merge and move next *)
             else aux 0 (merge (count+1) h acc) t
     in List.rev (aux 0 [] l);;
 
