c8547423c6979d24c976d10a97feb0fdf228c22c
Le Quoc Viet <vietlq85@gmail.com>
2015-01-24 19:37:35 +0000

    spaces

--- prob010_run_length_encode_a_list.ml
+++ prob010_run_length_encode_a_list.ml
@@ -21,8 +21,8 @@ can reduce the number of :: operations
 *)
 let encode list =
     let rec aux count acc = function
-      | [] -> [] (* Can only be reached if original list is empty *)
-      | [x] -> (count+1, x) :: acc
-      | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
-                              else aux 0 ((count+1,a) :: acc) t in
+        | [] -> [] (* Can only be reached if original list is empty *)
+        | [x] -> (count+1, x) :: acc
+        | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+            else aux 0 ((count+1,a) :: acc) t in
     List.rev (aux 0 [] list);;
