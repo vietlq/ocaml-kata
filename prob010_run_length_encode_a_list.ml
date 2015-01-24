e1d57c8751138e975d3f53a05d2388988d118095
Le Quoc Viet <vietlq85@gmail.com>
2015-01-24 15:33:56 +0000

    Added comment

--- prob010_run_length_encode_a_list.ml
+++ prob010_run_length_encode_a_list.ml
@@ -14,3 +14,15 @@ let run_length_encode l =
     in List.rev (aux [] l);;
 
 run_length_encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];;
+
+(*
+Alternative solution provided by http://ocaml.org/learn/tutorials/99problems.html
+can reduce the number of :: operations
+*)
+let encode list =
+    let rec aux count acc = function
+      | [] -> [] (* Can only be reached if original list is empty *)
+      | [x] -> (count+1, x) :: acc
+      | a :: (b :: _ as t) -> if a = b then aux (count + 1) acc t
+                              else aux 0 ((count+1,a) :: acc) t in
+    List.rev (aux 0 [] list);;
