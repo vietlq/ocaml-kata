6a5b3ba173ba3b27447bcb68f44a08b04b035195
Le Quoc Viet <vietlq85@gmail.com>
2015-01-25 00:08:20 +0000

    Added another version of decode

--- prob012_decode_run_length_encoded_list.ml
+++ prob012_decode_run_length_encoded_list.ml
@@ -20,3 +20,18 @@ let decode l =
     in List.rev (List.fold_left folder [] l);;
 
 (decode [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")]) = ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]
+
+(*
+This can be done without List.fold_left as well
+*)
+let decode2 l =
+    let rec make_list n x acc =
+        if n < 1 then acc
+        else make_list (n-1) x (x::acc)
+    in let rec aux acc = function
+        | [] -> acc
+        | One x::t -> aux (x::acc) t
+        | Many (n, x)::t -> aux (make_list n x acc) t
+    in List.rev (aux [] l)
+
+(decode2 [Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")]) = ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]
