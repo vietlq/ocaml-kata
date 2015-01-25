fe4bb57a7f3deda5fa91e53e6f828c57ac9ca87c
Le Quoc Viet <vietlq85@gmail.com>
2015-01-25 10:00:04 +0000

    Reset the counter to avoid using mod

--- prob016_drop_every_nth_from_list.ml
+++ prob016_drop_every_nth_from_list.ml
@@ -5,9 +5,9 @@ Problem 16: Drop every N'th element from a list.
 let drop l n =
     let rec aux acc count n = function
         | [] -> acc
-        | h::t -> if count mod n = 0
-            (* Skip every N-th element *)
-            then aux acc (count+1) n t
+        | h::t -> if count = n
+            (* Skip every N-th element, reset the counter *)
+            then aux acc 1 n t
             else aux (h::acc) (count+1) n t
     (* If the n < 1, return the whole list back *)
     in let helper l n = if n < 1 then l
